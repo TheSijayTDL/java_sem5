f7b20c33eb7ea3f8facae8b13c9975f27c0e0744
Ruslans Golubevs <s21golurusl@vea.lv>
2023-04-20 16:02:41 +0300

    Thymeleaf stuff

--- src/main/java/lv/venta/controllers/FirstController.java
+++ src/main/java/lv/venta/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package lv.venta.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -10,6 +11,12 @@ public class FirstController {
 	public String getHelloFunc() {
 		System.out.println("test");
 		return "hello-page"; // html page
+	}
+	
+	@GetMapping("/msg") // localhost:8080/msg
+	public String getMsgFunc(Model model) {
+		model.addAttribute("message", "Hello World!");
+		return "msg-page"; // html page
 	}
 	
 }
